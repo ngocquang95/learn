b6e6206e51db97156e365216b6602f0fbf664f23
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-05 22:24:55 +0700

    origin/feature/git-intellij/005

--- study/Student.java
+++ study/Student.java
@@ -3,11 +3,24 @@ package study;
 public class Student {
     private int studentId;
 
+    /**
+     * property5
+     */
+    private int property5;
+
     public int getStudentId() {
         return studentId;
     }
 
     public void setStudentId(int studentId) {
         this.studentId = studentId;
+    }
+
+    public int getProperty5() {
+        return property5;
+    }
+
+    public void setProperty5(int property5) {
+        this.property5 = property5;
     }
 }
