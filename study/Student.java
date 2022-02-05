01dd51f758efa428cc4bfc670ff9dd0791cb8481
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-05 19:26:45 +0700

    master

---
+++ study/Student.java
@@ -0,0 +1,13 @@
+package study;
+
+public class Student {
+    private int studentId;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
