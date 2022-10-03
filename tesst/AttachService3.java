29e69159fed81d1a9fbe9550484cbcd0e532bb9f
ngocquang95 <66203299+ngocquang95@users.noreply.github.com>
2022-10-03 22:52:49 +0700

    Update AttachService3.java

--- tesst/AttachService3.java
+++ tesst/AttachService3.java
@@ -7,10 +7,38 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-public             abstract        class     attachService {
+public class attachService {
     private int AttachServiceId = 1;
     private static final   String attach_ServiceName         = new String("ABC")       ;
     private Double attach$ServiceCost;
     private Integer attach_serviceUnit       ;
-    private final String attachServiceStatus;
+    private final String attachServiceStatus = "hello";
+
+    public int getAttachServiceId() {
+        return AttachServiceId;
+    }
+
+    public void setAttachServiceId(int attachServiceId) {
+        AttachServiceId = attachServiceId;
+    }
+
+    public Double getAttach$ServiceCost() {
+        return attach$ServiceCost;
+    }
+
+    public void setAttach$ServiceCost(Double attach$ServiceCost) {
+        this.attach$ServiceCost = attach$ServiceCost;
+    }
+
+    public Integer getAttach_serviceUnit() {
+        return attach_serviceUnit;
+    }
+
+    public void setAttach_serviceUnit(Integer attach_serviceUnit) {
+        this.attach_serviceUnit = attach_serviceUnit;
+    }
+
+    public String getAttachServiceStatus() {
+        return attachServiceStatus;
+    }
 }
