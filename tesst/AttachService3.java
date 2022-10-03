ec5c5c5107033451045ff64389620704dd29f78a
ngocquang95 <66203299+ngocquang95@users.noreply.github.com>
2022-10-03 22:35:50 +0700

    Create AttachService3.java

---
+++ tesst/AttachService3.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public             abstract        class     attachService {
+    private int AttachServiceId = 1;
+    private final String attach_ServiceName         = new String("ABC")       ;
+    private Double attach$ServiceCost;
+    private Integer attach_serviceUnit       ;
+    private final String attachServiceStatus;
+}
